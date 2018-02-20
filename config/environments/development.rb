e968f5edf23b9b9beafc00dc3bc334c14f8d0466
pittore <pittore44@yahoo.com>
2018-02-20 04:47:12 +0000

    new bundle install without prod after changes to config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   
-  config.web_console.whitelisted_ips =   '0.0.0.0/0.0.0.0'
-
-  # Do not eager load code on boot.
+# Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
